f507f387bbb2c3e6396b73907f6a25aa87fcda01
ojer <sonamu@foxmail.com>
2021-10-27 17:52:48 +0800

    'update'

--- GenCatalogue.java
+++ GenCatalogue.java
@@ -6,23 +6,21 @@ import java.io.IOException;
 public class GenCatalogue {
     public static void main(String[] args) throws IOException {
         String pageFolder = "page";
-        String cataloguePrefix = "1. [";
-        String catalogueSplicing = "](./" + pageFolder + "/";
-        String catalogueSuffix = ".html)";
+        String includes = "_includes/catalogue.html";
+        String cataloguePrefix = "<li><a href=\"./" + pageFolder + "/";
+        String catalogueSplicing = ".html\">";
+        String catalogueSuffix = "</a></li>)";
 
         File pages = new File(pageFolder);
-        File index = new File("catalogue.md");
+        File index = new File(includes);
 
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(index));
-        bufferedWriter.write("> catalogue");
-        bufferedWriter.flush();
-
         File[] files = pages.listFiles();
         assert files != null;
         for (File mdFile : files) {
             bufferedWriter.newLine();
             String name = mdFile.getName();
-            name = name.substring(0,name.lastIndexOf("."));
+            name = name.substring(0, name.lastIndexOf("."));
             bufferedWriter.write(cataloguePrefix + name + catalogueSplicing + name + catalogueSuffix);
             bufferedWriter.flush();
         }
