1d619de124dc1d632d707b4c55c4e181f07dc2f5
ojer <sonamu@foxmail.com>
2021-10-28 10:35:31 +0800

    'update'

--- GenCatalogue.java
+++ GenCatalogue.java
@@ -11,7 +11,7 @@ public class GenCatalogue {
     private static final String UL_END = "</ul>";
     private static final String LI_STA = "<li>";
     private static final String LI_END = "</li>";
-    private static final String CATALOGUE_PREFIX = LI_STA + "<a href=\"{{ '/" + PAGE_FOLDER + "/";
+    private static final String CATALOGUE_PREFIX = LI_STA + "<a href=\"{{ '/";
     private static final String CATALOGUE_SPLICING = ".html' }}\">";
     private static final String CATALOGUE_SUFFIX = "</a>" + LI_END;
 
@@ -21,29 +21,30 @@ public class GenCatalogue {
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(index));
         File[] files = pages.listFiles();
         assert files != null;
-        write(files, bufferedWriter);
+        write(files, PAGE_FOLDER, bufferedWriter);
         bufferedWriter.close();
     }
 
 
-    static void write(File[] files, BufferedWriter bufferedWriter) throws IOException {
+    static void write(File[] files, String dir, BufferedWriter bufferedWriter) throws IOException {
         for (File mdFile : files) {
             if (mdFile.isFile()) {
                 bufferedWriter.newLine();
                 String name = mdFile.getName();
                 name = name.substring(0, name.lastIndexOf("."));
-                bufferedWriter.write(CATALOGUE_PREFIX + name + CATALOGUE_SPLICING + name + CATALOGUE_SUFFIX);
+                bufferedWriter.write(CATALOGUE_PREFIX + dir + "/" + name + CATALOGUE_SPLICING + name + CATALOGUE_SUFFIX);
                 bufferedWriter.flush();
-            } else {
+            } else if (mdFile.isDirectory()) {
                 File[] listFiles = mdFile.listFiles();
                 if (listFiles != null) {
+                    String name = mdFile.getName();
                     bufferedWriter.newLine();
                     bufferedWriter.write(LI_STA);
-                    bufferedWriter.write(mdFile.getName());
+                    bufferedWriter.write(name);
                     bufferedWriter.newLine();
                     bufferedWriter.write(UL_STA);
                     bufferedWriter.flush();
-                    write(listFiles, bufferedWriter);
+                    write(listFiles, dir + "/" + name, bufferedWriter);
                     bufferedWriter.newLine();
                     bufferedWriter.write(UL_END);
                     bufferedWriter.newLine();
