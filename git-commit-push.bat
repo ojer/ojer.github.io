955e57d2d3c622472db944bf703ef14c53ea610c
ojer <sonamu@foxmail.com>
2021-10-28 09:02:21 +0800

    'update'

--- git-commit-push.bat
+++ git-commit-push.bat
@@ -1,26 +1,16 @@
 @echo off
 
-echo javac
+echo --start
+echo --javac
 javac GenCatalogue.java
 
-echo GenCatalogue
+echo --GenCatalogue
 java GenCatalogue
 
-echo git commit
+echo --git commit
 git add .
 git commit -m 'update'
 git push origin master
 
-IF %errorlevel% NEQ 0 GOTO ERROR
-IF %errorlevel% EQU 0 GOTO OK
-
-:OK
-@cmd /c
-GOTO END
-
-: ERROR
-@cmd /k
-GOTO END
-
-:END
-
+echo --end
+pause >null
